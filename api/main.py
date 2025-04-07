36cfc0a755fecc00da8a1d1e7d762a119eef8524
mohammed elgasim <mohammedalgasim123@gmail.com>
2025-04-07 14:11:29 +0200

    add new change

--- api/main.py
+++ api/main.py
@@ -50,8 +50,6 @@ async def upload_cv(file: UploadFile = File(...)):
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 # تشغيل السيرفر
-port = int(os.getenv("PORT", 8000))  # استخدام المنفذ المحدد من Render أو 8000 افتراضيًا
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=port)
